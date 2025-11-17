a35428635bcb04cfeee70801ae81858aa12afec1
tobielegbeleye-cmd <tobi.elegbeleye@vroomng.com>
2025-11-17 22:50:23 +0200

    sequalize

--- Models/splitbill.js
+++ Models/splitbill.js
@@ -1,57 +1,36 @@
-const pool = require('../dbconnect');
-const bcrypt = require('bcryptjs');
+const {Sequelize, DataTypes} = require("sequelize");
 
-class SplitBill {
-    static async create(bill_id,user_id, amount_to_pay, method,description,date_due,images) {
+const sequelize = require('../Config/sequalize_db');
 
-        const sql = 'INSERT INTO `bill_splits`(`bill_id`, `user_id`, `amount_to_pay`, `method`, `description`, `date_due`, `images`) VALUES VALUES (?,?,?,?,?,?,?)'
-        const [result] = await pool.execute(
-            sql,[bill_id, user_id, amount_to_pay, method, description,date_due,images]
-        );
+const SplitBill = sequelize.define("splitbill", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        bill_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+        },
 
+        amount_to_pay: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+        },
+        paid: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            unique: true,
+        },
+});
 
-        return result.insertId;
-    }
-
-    static async updateSplitBill(user_id,bill_id,amount_paid, last,username,rUsername) {
-
-        //const sql = 'INSERT INTO `users`(`email`,`phone`, `password_hash`) VALUES (?,?,?,?,?)'
-        const [result] = await pool.execute(
-            'UPDATE `bill_splits` SET first_name = ?, last_name =?, username = ?, rusername = ? WHERE id = ?',
-            [first, last, username,rUsername, id]
-        );
-
-        console.log(result);
-
-        return result.insertId;
-    }
-
-    static async getSpliBills(bill_id) {
-        const [rows] = await pool.execute(
-            'SELECT * FROM bill_splits WHERE id = ?',
-            [bill_id]
-        );
-        return rows[0];
-    }
-
-    
-
-    static async findById(id) {
-        const [rows] = await pool.execute(
-            'SELECT * FROM bill_splits WHERE id = ?',
-            [id]
-        );
-        return rows[0];
-    }
-
-    static async Delete(id) {
-        const [rows] = await pool.execute(
-            'DELETE FROM `bill_splits` WHERE id = ?',
-            [id]
-        );
-        return rows[0];
-    }
-
-}
-
-module.exports = SplitBill;
+sequelize.sync().then(() => {
+   console.log('SplitBill table created successfully!');
+}).catch((error) => {
+   console.error('Unable to create table : ', error);
+});
